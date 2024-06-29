6f7c0e7547552a71572b6bde8376d56b68f21669
Habtemaraim Bereket <habteshbaki2014@gmail.com>
2024-06-29 05:51:08 -0700

    add some stripe payment methods

---
+++ Backend/Controller/paymentController.js
@@ -0,0 +1,24 @@
+const stripe = require('stripe')(process.env.STRIPE_SECREATE_KEY)
+
+
+exports.createPayment = async ( req, res) => {
+    try{
+         const {totalPrice} = req.body
+
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(totalPrice * 100),
+      currency:'USD'
+    })
+
+    res.status(200).json({
+        clientSecret: paymentIntent.client_secret,
+    })
+    }catch(err){
+       res.status(400).json({
+        status:'fail',
+        message:err.message
+       })
+    }
+   
+}
--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const userRoute = require('./Routes/userRoute')
 const productRoute = require('./Routes/productRoute')
 const orderRoute = require('./Routes/orderRoute')
+const paymentRoute = require('./Routes/paymentRoute')
 
 const app = express();
 // app.use(morgan)
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/orders', orderRoute)
+app.use('/api/payments', paymentRoute)
 
 
 module.exports = app
